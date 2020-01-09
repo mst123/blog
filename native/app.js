c5ceaab31012e88d8b7112abb2ba6177d4b51ecd
mashitu <mashitu@piesat.cn>
2020-01-09 15:57:24 +0800

    session解决登陆 无redis版本

--- native/app.js
+++ native/app.js
@@ -2,10 +2,91 @@ const querystring = require('querystring')
 const handleUserRounter = require('./src/router/user')
 const handleBlogRounter = require('./src/router/blog')
 
-//session 数据
+//定义 服务端session
 const SESSION_DATA = {}
 
-//用于处理 post data
+const serverHandle = (req, res) => {
+  res.setHeader('Content-type', 'application/json') //业界规范。。。
+  req.path = req.url.split('?')[0]
+
+  //解析客户端发来的cookie， 其中记录着sessionId
+  req.cookie = {}
+  const cookieStr = req.headers.cookie || ''  //k1=v1;k2=v2;
+  cookieStr.split(';').forEach(item => {
+    if(item){
+      req.cookie[item.split('=')[0]] = item.split('=')[1] 
+    }
+  });
+  console.log(req.cookie);
+
+  //解析session
+  let needSetCookie = false
+  let userId = req.cookie.userId //sessionId 
+  if(userId){  
+    if(!SESSION_DATA[userId]){ //有sessionId  但是session中没有用户记录
+      SESSION_DATA[userId] = {}
+    }
+  }else{  //没有sessionId 需要生成sessionId，并记录在cookie中
+    needSetCookie = true
+    userId = Date.now() + '_' + Math.random() 
+    SESSION_DATA[userId] = {}
+  }
+  req.session = SESSION_DATA[userId] //将session中记录的用户信息，放置在req中，以便后续使用
+
+  //处理get请求参数
+  req.query = querystring.parse(req.url.split('?')[1])
+
+  //处理post请求参数
+  getPostData(req).then((postData) => {
+    req.body = postData
+    //处理blog相关接口
+    const blogResult = handleBlogRounter(req, res)
+    if(blogResult){
+      blogResult.then((blogData) => {
+        if(needSetCookie){
+          //server端操作cookie
+          res.setHeader(
+            'Set-Cookie',
+            `userId=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`  //path=/ 是cookie适用于所有根目录是/的请求域  
+            //httpOnly 只存在于server端的cookie属性 前端无法查看（浏览器中可以看到）与修改
+            // 前端也可以定义username 但是和server端的username 分别存在 
+          )
+        }
+        res.end( JSON.stringify(blogData) )
+      })
+      return
+    }
+    //处理user相关接口
+    const userResult = handleUserRounter(req, res)
+    if(userResult){
+      userResult.then((userData) => {
+        if(needSetCookie){
+          //server端操作cookie
+          res.setHeader(
+            'Set-Cookie',
+            `userId=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`  //path=/ 是cookie适用于所有根目录是/的请求域  
+          )
+        }
+        res.end( JSON.stringify(userData) )
+      })
+      return
+    }
+    //未命中路由，返回404
+    res.writeHead(404, { //不常用
+      'Content-type': 'text/plain'
+    })
+    res.write('404 not found \n') //不常用
+    res.end()
+  })
+}
+//工具-------获取 cookie 的过期时间,此为一天
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24*60*60*1000))
+  console.log(d.toGMTString());
+  return d.toGMTString()  //cookie的时间格式
+}
+//工具-------用于处理 post data
 const getPostData = (req) => {
   return new Promise((resolve, reject) => {
     if(req.mehhod == 'GET'){
@@ -25,69 +106,8 @@ const getPostData = (req) => {
         resolve({})
       }else{
         resolve(JSON.parse(postData))
-      }
-      
+      }    
     })
-  })
-}
-const serverHandle = (req, res) => {
-  res.setHeader('Content-type', 'application/json') //业界规范。。。
-  req.path = req.url.split('?')[0]
-
-  //解析cookie
-  req.cookie = {}
-  const cookieStr = req.headers.cookie || ''  //k1=v1;k2=v2;
-  cookieStr.split(';').forEach(item => {
-    if(item){
-      req.cookie[item.split('=')[0]] = item.split('=')[1] 
-    }
-  });
-  console.log(req.cookie);
-
-  //解析session
-  const userId = req.cookie.userId
-  if(userId){
-    if(!SESSION_DATA[userId]){
-      SESSION_DATA[userId] = {}
-    }
-  }else{
-    userId = Date.now() + '_' + Math.random() 
-    SESSION_DATA[userId] = {}
-  }
-  req.session = SESSION_DATA[userId]
-
-  //处理get请求参数
-  req.query = querystring.parse(req.url.split('?')[1])
-
-  //处理post请求参数
-  getPostData(req).then((postData) => {
-    req.body = postData
-
-    const blogResult = handleBlogRounter(req, res)
-    if(blogResult){
-      blogResult.then((blogData) => {
-        res.end(
-          JSON.stringify(blogData)
-        )
-      })
-      return
-    }
-
-    const userResult = handleUserRounter(req, res)
-    if(userResult){
-      userResult.then((userData) => {
-        res.end(
-          JSON.stringify(userData)
-        )
-      })
-      return
-    }
-    //未命中路由，返回404
-    res.writeHead(404, { //不常用
-      'Content-type': 'text/plain'
-    })
-    res.write('404 not found \n') //不常用
-    res.end()
   })
 }
 module.exports = serverHandle
--- native/src/router/user.js
+++ native/src/router/user.js
@@ -1,13 +1,6 @@
 const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
-//获取 cookie 的过期时间
-const getCookieExpires = () => {
-  const d = new Date()
-  d.setTime(d.getTime() + (24*60*60*1000))
-  console.log(d.toGMTString());
-  return d.toGMTString()  //cookie的时间格式
-}
 const handleUserRoueter = (req, res) => {
   const method = req.method
   //登陆
@@ -25,13 +18,11 @@ const handleUserRoueter = (req, res) => {
     const { username, password } = req.query
     return login(username, password).then(data => {
       if(data.username){
-        //server端操作cookie
-        res.setHeader(
-          'Set-Cookie',
-          `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`  //path=/ 是cookie适用于所有根目录是/的请求域  
-          //httpOnly 只存在于server端的cookie属性 前端无法查看（浏览器中可以看到）与修改
-          // 前端也可以定义username 但是和server端的username 分别存在 
-        )
+        // 设置 session
+        req.session.username = data.username
+        req.session.realname = data.realname
+        console.log('req.session ');
+        console.log(req.session);
         return new SuccessModel({
           username: data.username
         })
@@ -44,10 +35,10 @@ const handleUserRoueter = (req, res) => {
   if(method == 'GET' && req.path == '/api/user/login-test'){
     console.log(req.cookie);
     
-    if(req.cookie.username){
+    if(req.session.username){
       return Promise.resolve(
         new SuccessModel({
-          username: req.cookie.username
+          session: req.session
         })
       ) 
     }else{
